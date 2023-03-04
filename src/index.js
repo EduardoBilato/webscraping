71fcafbddbcfea9b63a49d91b1e0f134f3fe7595
Eduardo Bilato <eduardobilato@gmail.com>
2023-03-04 10:32:37 -0300

    feat puppeteer-cluster

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const pup = require('puppeteer');
+const { Cluster } = require('puppeteer-cluster');
 
 const url = 'http://www.mercadolivre.com.br';
 const searchFor = 'macbook';
@@ -12,91 +13,100 @@ async function clickNavegate(page, selector, searchFor) {
     ]);
 }
 
-async function extractInfo(page, selector) {
-    await page.waitForSelector(selector);
-    const valor = await page.$eval(selector, element => element?.innerText);
-    return valor;
-}
+async function scrapping({ page, data: { link, count } }) {
 
-// entrada: lista de objetos com nome e seletor
-// saída: agrega uma propriedade "valor" para cada elemento da lista
-async function extractListInfo(page, list) {
-    const newList = [];
-
-    await list.forEach(async ({ chave, selector }, idx) => {
-        // const valor = await extractInfo(page, selector);
-
-        await page.waitForSelector(selector);
-        const valor = await page.$eval(selector, element => element?.innerText);
-
-        const obj = { chave, valor };
-        console.log(`Pagina ${idx}`, obj);//teste
-        newList.push(obj);
-    });
-
-    console.log('======> newlist', newList);//teste
-    return newList;
-}
-
-(async () => {
-    const browser = await pup.launch({
-        headless: false //aqui é que exibe o navegador
-    });
-    const page = await browser.newPage();
-
-    await page.goto(url);
-
-    const selector = '.nav-search-input';
-    const selectTorList = '.ui-search-result__content';
-    // const selectTorList = '.ui-search-layout__item';
-    //  ui-search-link
-
-    await page.waitForSelector(selector);
-    await clickNavegate(page, selector, searchFor);
-
-    const links = await page.$$eval(`${selectTorList}`
-        , el => el
-            .map(link => link.href))
-
-    for (const link of links) {
-
-        await page.goto(link);
-
-        const indexadores = [{
+    const indexadores = [
+        {
             chave: 'title',
             selector: '.ui-pdp-title',
+            itExists: true,
         },
         {
             chave: 'price',
             selector: '.andes-money-amount__fraction',
+            itExists: true,
         },
         {
             chave: 'cents',
             selector: '.andes-money-amount__cents',
+            itExists: true,
         },
-        ]
+        {
+            chave: 'seller',
+            selector: '.ui-pdp-seller__link-trigger',
+            itExists: false,
+        }
+    ]
 
-        // const lista = await extractListInfo(page, indexadores)
-        //     .then((data) => console.log('data', data))//teste
-        const selector1 = indexadores[0].selector;
-        const selector2 = indexadores[1].selector;
-        const selector3 = indexadores[2].selector;
+    const getInfo = async (selector) => await page.$eval(selector, element => element?.innerText); // usado quando sabemos que o seletor existe
+    const tryGetInfo = async (selector) => await page.evaluate((selector) => {
+        const el = document.querySelector(selector);
+        return el ? el.innerText : '';
+    }); // usado quando NÃO sabemos se o seletor existe
 
+    const obj = { link, count };
 
-        await page.waitForSelector(selector1);
-        const chave = await page.$eval(selector1, element => element?.innerText);
-        const price = await page.$eval(selector2, element => element?.innerText);
-        const cents = await page.$eval(selector3, element => element?.innerText);
-        const seller = await page.evaluate(() => {
-            const el = document.querySelector('.ui-pdp-seller__link-trigger');
-            return el ? el.innerText : '';
+    await page.goto(link);//, { waitUntil: 'networkidle2' });//aguarda o carregamento da página
+    await page.waitForSelector(indexadores[0].selector);//aguardar o seletor
+    
+    for (const { chave, selector, itExists } of indexadores) {
+        const value = itExists
+            ? await getInfo(selector)
+            : await tryGetInfo(selector);
+        obj[chave] = value;
+    };
+
+    console.log(obj);
+
+    return obj;
+}
+
+async function main() {
+
+    const pid = process.pid;
+
+    try {
+        const cluster = await Cluster.launch({
+            concurrency: Cluster.CONCURRENCY_CONTEXT,
+            maxConcurrency: 15,
         });
 
-        const obj = { chave, price, cents, seller, link }
+        const browser = await pup.launch({
+            headless: true //aqui é que exibe o navegador
+        });
+        const page = await browser.newPage();
 
-        console.log('\n\n===> obj', obj);//teste
+        await page.goto(url);
+
+        const selector = '.nav-search-input';
+        const selectTorList = '.ui-search-result__content';
+
+        await page.waitForSelector(selector);
+        await clickNavegate(page, selector, searchFor);
+
+        //OBS: $$eval, avalia vários seletores
+        //ao passo que $eval, avalia somente um seletor
+        const links = await page.$$eval(`${selectTorList}`
+            , el => el
+                .map(link => link.href))
+
+        console.log(`Existem ${links.length} resultados`);
+        let count = 1;
+        for (const link of links) {
+            await cluster.queue({ link, count });
+            count++;
+        }
+        await cluster.task(scrapping);
+
+        await cluster.idle(); //esperar todas as tarefas terminarem
+        await cluster.close(); //finaliza o cluster
+        await browser.close() //finaliza o browser
+
+    }
+    catch (err) {
+        console.error(`${pid} has broken! ${err.stack}`);
     }
 
-    // await browser.close();
+}
 
-})();
+main();
